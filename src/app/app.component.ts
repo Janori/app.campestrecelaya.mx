94b71ded2120a8da2a8c4d9e672db46b0a54cc5e
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2018-01-05 03:01:55 -0600

    Tarjetas en home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { LoginPage, MenuPage, ProfilePage, SwimmingPoolPage } from '../pages/ind
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = SwimmingPoolPage;
+  rootPage:any = MenuPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
